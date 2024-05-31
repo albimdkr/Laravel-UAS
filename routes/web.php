367832665419ec07f1dfeec4ce4b0f5fd2fef523
albimdkr <albimudakar04@gmail.com>
2024-06-01 01:12:06 +0700

    feat: route signin and signup

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,11 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 
+// Default Route
 Route::get('/', function(){
     return view('Welcome', ["title" => "Welcome"]);
 });
 
+// Routing to other page
 Route::resource('/signin', AuthController::class);
 Route::resource('/signup', AuthController::class);
