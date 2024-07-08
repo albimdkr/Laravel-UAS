50823b3cc6b2281f9890cbf39d761024a5cf2c02
albimdkr <albimudakar04@gmail.com>
2024-07-08 19:08:21 +0700

    feat: add clients migrations db

--- database/migrations/2024_07_07_163315_clients.php
+++ database/migrations/2024_07_07_163315_clients.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::create('users', function (Blueprint $table) {
+        Schema::create('clients', function (Blueprint $table) {
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->enum('role', ['admin','operator', 'engineer', 'audit', 'user'])->default('user');
-            $table->rememberToken();
+            $table->text('manage_scope')->nullable();
+            $table->string('contact_name')->nullable();
+            $table->string('contact_email')->nullable();
+            $table->string('contact_phone')->nullable();
             $table->timestamps();
         });
     }
@@ -28,6 +28,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('users');
+        //
     }
 };
