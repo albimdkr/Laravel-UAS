1fad47b7018bd22b00ca252d95cf2c808ca618be
albimdkr <albimudakar04@gmail.com>
2024-07-09 08:07:16 +0700

    feat: update footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,7 +1,7 @@
 <footer class="sticky-footer bg-white">
     <div class="container my-auto">
       <div class="copyright text-center my-auto">
-        <span>Copyright © NET-TRACKER 2024</span>
+        <span>Copyright © NET-TRACKER 2024 by <a href="https://albimdkr.github.io/portfolio/" target="_blank">Albi Mudakar <i class="fas fa-external-link-alt"></i></a></span>
       </div>
     </div>
   </footer>
