cf1f26a315fd2055950bf7135a34f7481b9fe22d
albimdkr <albimudakar04@gmail.com>
2024-07-09 14:08:52 +0700

    feat: modifying views issue on create page

--- resources/views/issues/create.blade.php
+++ resources/views/issues/create.blade.php
@@ -13,26 +13,42 @@
                     <option value="{{ $client->id }}">{{ $client->name }}</option>
                 @endforeach
             </select>
+            @error('client_id')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
             <label for="issue">Issue</label>
             <input type="text" class="form-control" id="issue" name="issue" required>
+            @error('issue')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
             <label for="status">Status</label>
             <select class="form-control" id="status" name="status" required>
-                <option value="open">Open</option>
-                <option value="in-progress">In Progress</option>
-                <option value="closed">Closed</option>
+                <option value="">Select Status</option>
+                <option value="Open">Open</option>
+                <option value="In Progress">In Progress</option>
+                <option value="Closed">Closed</option>
             </select>
+            @error('status')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
-            <label for="start_date">Start Date</label>
-            <input type="date" class="form-control" id="start_date" name="start_date">
+            <label for="date_start_tshoot">Start Date</label>
+            <input type="datetime-local" class="form-control" id="date_start_tshoot" name="date_start_tshoot" required>
+            @error('date_start_tshoot')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
         <div class="form-group">
-            <label for="end_date">End Date</label>
-            <input type="date" class="form-control" id="end_date" name="end_date">
+            <label for="date_end_tshoot">End Date</label>
+            <input type="datetime-local" class="form-control" id="date_end_tshoot" name="date_end_tshoot">
+            @error('date_end_tshoot')
+                <div class="text-danger">{{ $message }}</div>
+            @enderror
         </div>
         <button type="submit" class="btn btn-primary">Create Issue</button>
     </form>
