21d636746de102e2f94d34f835b8e2f72fc7a0fa
albimdkr <albimudakar04@gmail.com>
2024-07-10 22:13:05 +0700

    fix: editing UI and fix error undefined

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,13 +7,13 @@
     <div class="container-fluid">
       <!-- Page Heading -->
       <div class="d-sm-flex align-items-center justify-content-between mb-4">
-          <h1 class="h3 mb-0 text-gray-800">Welcome | {User}</h1>
+          <h1 class="h3 mb-0 text-gray-800">Welcome Back!</h1>
       </div>
 
         {{-- Card In --}}
         <hr>
         <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Products Income</h1>
+            <h1 class="h3 mb-0 text-gray-800">List Issue Status</h1>
             <a href="{{ route('products_in.totalProductsPrint') }}" class="btn btn-primary">Print <i class="fas fa-print"></i></a>
         </div>
         <div class="row">
@@ -24,7 +24,7 @@
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-info text-uppercase mb-1">Open</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalProductsIn }}</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalOpen }}</div>
                             </div>
                             <div class="col-auto">
                                 <i class="fas fa-plus-circle fa-2x text-info"></i>
@@ -41,7 +41,7 @@
                         <div class="row no-gutters align-items-center">
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-warning text-uppercase mb-1">In Progress</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalElectronicIn }}</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalInProgress }}</div>
                             </div>
                             <div class="col-auto">
                             <i class="fas fa-circle-notch fa-2x text-warning"></i>
@@ -61,7 +61,7 @@
                                 </div>
                                 <div class="row no-gutters align-items-center">
                                     <div class="col-auto">
-                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $totalToolIn }}</div>
+                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $totalPending }}</div>
                                     </div>
                                 </div>
                             </div>
@@ -81,7 +81,7 @@
                             <div class="col mr-2">
                                 <div class="text-xs font-weight-bold text-success text-uppercase mb-1">
                                     Closed</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalFurnitureIn }}</div>
+                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalClosed }}</div>
                             </div>
                             <div class="col-auto">
                             <i class="fas fa-check-circle fa-2x text-success"></i>
@@ -92,162 +92,46 @@
             </div>
         </div>
 
-        {{-- Chart In --}}
+        {{-- Chart Issue --}}
         <div class="row">
             <!-- Pie Chart -->
             <div class="col-xl-12 col-lg-5">
-                <div class="card shadow mb-4" style="display: flex;"">
+                <div class="card shadow mb-4" style="display: flex;">
                     <!-- Card Header - Dropdown -->
                     <div
                         class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Category Overview Income</h6>
+                        <h6 class="m-0 font-weight-bold text-primary">Overview Status</h6>
                     </div>
                     <!-- Card Body -->
                     <div class="card-body">
                         <div class="chart-pie pt-4 pb-2">
-                            <canvas id="myPieChartIn"></canvas>
+                            <canvas id="myPieChartIssues"></canvas>
                         </div>
                         <div class="mt-4 text-center small">
                             <span class="mr-2">
-                                <i class="fas fa-circle text-primary"></i> Electronics
+                                <i class="fas fa-circle text-info"></i> Open
                             </span>
                             <span class="mr-2">
-                                <i class="fas fa-circle text-success"></i> Tools
+                                <i class="fas fa-circle text-warning"></i> In Progress
                             </span>
                             <span class="mr-2">
-                                <i class="fas fa-circle text-info"></i> Furniture
+                                <i class="fas fa-circle text-danger"></i> Pending
+                            </span>
+                            <span class="mr-2">
+                                <i class="fas fa-circle text-success"></i> Closed
                             </span>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-        
-        {{-- Card Out --}}
-        <hr>
-        <div class="d-sm-flex align-items-center justify-content-between mb-4">
-            <h1 class="h3 mb-0 text-gray-800">Products Outcome</h1>
-            <a href="{{ route('products_out.totalProductsOutPrint') }}" class="btn btn-warning">Print <i class="fas fa-print"></i></a>
-        </div>
-        <div class="row">
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-secondary shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-secondary text-uppercase mb-1">Total Products</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalProductsOut }}</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-boxes fa-2x text-secondary"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-primary shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-primary text-uppercase mb-1">Total Electronics</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalElectronicOut }}</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-boxes fa-2x text-primary"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Earnings (Monthly) Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-success shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-success text-uppercase mb-1">Total Tools
-                                </div>
-                                <div class="row no-gutters align-items-center">
-                                    <div class="col-auto">
-                                        <div class="h5 mb-0 mr-3 font-weight-bold text-gray-800">{{ $totalToolOut }}</div>
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-boxes fa-2x text-success"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-
-            <!-- Pending Requests Card Example -->
-            <div class="col-xl-3 col-md-6 mb-4">
-                <div class="card border-left-info shadow h-100 py-2">
-                    <div class="card-body">
-                        <div class="row no-gutters align-items-center">
-                            <div class="col mr-2">
-                                <div class="text-xs font-weight-bold text-info text-uppercase mb-1">
-                                    Total Furniture</div>
-                                <div class="h5 mb-0 font-weight-bold text-gray-800">{{ $totalFurnitureOut }}</div>
-                            </div>
-                            <div class="col-auto">
-                                <i class="fas fa-boxes fa-2x text-info"></i>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-
-        {{-- Chart Out --}}
-        <div class="row">
-            <!-- Pie Chart -->
-            <div class="col-xl-12 col-lg-5">
-                <div class="card shadow mb-4" style="display: flex;"">
-                    <!-- Card Header - Dropdown -->
-                    <div
-                        class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                        <h6 class="m-0 font-weight-bold text-primary">Category Overview Outcome</h6>
-                    </div>
-                    <!-- Card Body -->
-                    <div class="card-body">
-                        <div class="chart-pie pt-4 pb-2">
-                            <canvas id="myPieChartOut"></canvas>
-                        </div>
-                        <div class="mt-4 text-center small">
-                            <span class="mr-2">
-                                <i class="fas fa-circle text-primary"></i> Electronic
-                            </span>
-                            <span class="mr-2">
-                                <i class="fas fa-circle text-success"></i> Tools
-                            </span>
-                            <span class="mr-2">
-                                <i class="fas fa-circle text-info"></i> Furniture
-                            </span>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
     <!-- /.container-fluid -->
 
     <script>
-        // In
-        var totalElectronicIn = {{ $totalElectronicIn }};
-        var totalToolIn = {{ $totalToolIn }};
-        var totalFurnitureIn = {{ $totalFurnitureIn }};
-
-        // Out
-        var totalElectronicOut = {{ $totalElectronicOut }};
-        var totalToolOut = {{ $totalToolOut }};
-        var totalFurnitureOut = {{ $totalFurnitureOut }};
+        // Issue
+        var totalOpen = {{ $totalOpen }};
+        var totalInProgress = {{ $totalInProgress }};
+        var totalPending = {{ $totalPending }};
+        var totalClosed = {{ $totalClosed }};
     </script>
 @endsection
